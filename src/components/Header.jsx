d7778b522834160b7ef8b62b07dd050b7703c350
shokhruh61 <shohruxmahmudov61@gmail.com>
2025-02-18 17:21:36 +0500

    bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/images/logo.svg";
 
 function Header() {
-  return (
-    <div>
-        
-    </div>
-  )
+  return;
+  <div>
+    <img src={Logo} alt="invoice app" />
+  </div>;
 }
 
-export default Header
+export default Header;
